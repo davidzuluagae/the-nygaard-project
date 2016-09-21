1b26d25fc5ac9652099ab078c0d501c6f42c81e9
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-09-21 17:33:02 -0600

    attempt at post endpoint with key verification. Issue #6

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 // Libs
 var express = require('express');
 var aws = require('aws-sdk');
+var bodyParser = require('body-parser');
 
 // Configuration
-const port = process.env.port || 8081; 
-const snsTopic = process.env.snsTopic;
-const apiKey = process.env.apiKey;
+const PORT = process.env.PORT || 8081; 
+const SNS_TOPIC = process.env.SNS_TOPIC;
+const API_KEY = process.env.API_KEY;
+const REGION = process.env.REGION || 'us-west-2';
 
 // The app
 var app = express();
 
+aws.config.update({region: REGION});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
   res.send( 'That was easy' );
+});
+
+app.post('/', function(req,res){
+  var sns = new aws.SNS();
+
+  if (req.body.apiKey == API_KEY){
+    sns.publish({
+      TargetArn: SNS_TOPIC, 
+      Message:"Test publish from api", 
+      Subject: "Test SNS publish"}, 
+      function(err,data){
+        if (err){
+          console.log("Error sending a message "+ err);
+          res.send("Error sending a message "+ err);
+        } else {
+          console.log("Sent message: "+ data.MessageId);
+          res.send("Sent message: "+ data.MessageId);
+        }
+    });
+
+  } else {
+  	res.send("Negative")
+  }
+
 });
 
 app.get('/health', function(req, res) {
   res.json({ "STATUS": "UP" });
 });
 
-app.listen(port);
-console.log("Server listening on: http://localhost:%s", port);
-console.log("Will publish events to: %s", snsTopic);
-console.log("API Key: %s", apiKey);
+app.listen(PORT);
+console.log("Server listening on: http://localhost:%s", PORT);
+console.log("Will publish events to: %s", SNS_TOPIC);
+console.log("API Key: %s", API_KEY);
 
 // Export the app for unit testing
 module.exports = app;
