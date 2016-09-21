db38add8ea3578d2563a3ccae8824b1bdc8292d6
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-09-21 12:37:01 -0600

    Issue #6 Updated to simple express app with two endpoints.

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
 
-//Lets define a port we want to listen to
 const PORT=8081; 
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end( JSON.stringify({ "STATUS": "UP" }) );
-}
+var app = express();
 
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.get('/', function(req, res) {
+  res.send( 'That was easy' );
 });
+
+app.get('/health', function(req, res) {
+  res.send( JSON.stringify({ "STATUS": "UP" }) );
+});
+
+app.listen(PORT);
+console.log("Server listening on: http://localhost:%s", PORT);
